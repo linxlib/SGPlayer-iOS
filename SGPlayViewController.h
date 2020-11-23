5a377120315f688805e70c61f19180b8b581562c
kirileec <sulinke1133@gmail.com>
2020-11-23 09:19:26 +0800

    Add files via upload

---
+++ SGPlayViewController.h
@@ -0,0 +1,17 @@
+//
+//  SGPlayViewController.h
+//  demo-ios
+//
+//  Created by Single on 2017/3/15.
+//  Copyright © 2017年 single. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGPlayViewController : UIViewController
+
+
+@property (nonatomic, copy) NSString *mediaPath;
+@property (nonatomic,copy) NSString *tit;
+
+@end
